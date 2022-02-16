f14ad02990be56c05f8dc7c32ad561c6d433554e
Agrard <gabor.kallai12@gmail.com>
2022-02-16 13:23:37 +0100

    3 feladat

--- Sorozatkiir2/Program.cs
+++ Sorozatkiir2/Program.cs
@@ -15,6 +15,34 @@ namespace Sorozatkiir2
             {
                 Console.Write(i + ", ");
             }
+
+            //3. feladat
+
+
+            bool igaz = false;
+            while (igaz == false)
+            {
+                Console.Write("\nKérek egy 1-től nagyobb számot 10-ig maximum: ");
+                int kezdes = Convert.ToInt32(Console.ReadLine());
+                if (kezdes < 1)
+                {
+                    Console.WriteLine("\nHibás megadás! 1-nél nagyobb számot adjon meg!");
+                }
+                else if (kezdes > 10)
+                {
+                    Console.WriteLine("\nHibás megadás! 10-nél kisebb számot adjon meg!");
+                }
+                else
+                {
+                    igaz = true;
+                    for (int i = kezdes; i <= 10; i++)
+                    {
+
+                        Console.Write(i + ", ");
+
+                    }
+                }
+            }
         }
     }
 }
